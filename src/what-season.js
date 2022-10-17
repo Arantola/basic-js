e722aa86f44784c9b4985e36d4e32304a7af2d33
Arseny Fedorchenko <Arantola@mail.ru>
2022-10-17 18:41:53 +0300

    feat: add what-season solution

--- src/what-season.js
+++ src/what-season.js
@@ -13,28 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 
 function getSeason(date) {
-  throw new NotImplementedError('Not implemented');
-  // if (!date) {
-  //   return 'Unable to determine the time of year!'
-  // }
+  if (date === undefined) return 'Unable to determine the time of year!';
 
-  // if (isNaN(date.getUTCHours())) {
-  //   throw new Error('Invalid date!');
-  // }
+  try { 
+    date.getTime();
+  }
+  catch { 
+    throw new Error('Invalid date!');
+  }
 
-  // let dateFromStr = new Date(date);
-  // let month = dateFromStr.getMonth();
-
-  // switch (month) {
-  //   case (0 || 1 || 11):
-  //     return 'winter';
-  //   case (2 || 3 || 4):
-  //     return 'spring';
-  //   case (5 || 6 || 7):
-  //     return 'summer';
-  //   case (8 || 9 || 10):
-  //     return 'autumn';
-  // }
+  let month = date.getMonth();
+  if (month > 1 && month < 5) return 'spring';
+  if (month > 4 && month < 8) return 'summer';
+  if (month > 7 && month < 11) return 'autumn';
+  return 'winter';
 };
 
 module.exports = {
